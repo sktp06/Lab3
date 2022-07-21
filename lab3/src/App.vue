e5b2e9b3a9700c757f51ead8049beb90a4a63c09
sktp06 <kantaporn_s@cmu.ac.th>
2022-07-21 22:51:26 +0700

    5.6

--- lab3/src/App.vue
+++ lab3/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/categories">Categories</router-link>
   </nav>
   <!-- <h1>Events For Good</h1> -->
   <!-- new element -->
--- lab3/src/router/index.js
+++ lab3/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
 import AboutView from '../views/AboutView.vue'
+import CategoriesView from '../views/CategoriesView.vue'
 
 const routes = [
   {
@@ -17,6 +18,16 @@ const routes = [
     // component: () =>
     //   import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
     component: AboutView
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: CategoriesView
   }
 ]
 

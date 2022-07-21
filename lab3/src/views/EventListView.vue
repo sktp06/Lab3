92b92d4ab69a2039e2d55c9d167144494bd11019
sktp06 <kantaporn_s@cmu.ac.th>
2022-07-22 00:03:00 +0700

    6.10

--- lab3/src/views/EventListView.vue
+++ lab3/src/views/EventListView.vue
@@ -1,13 +1,9 @@
 <template>
-  <h1>Events For Good</h1>
+  <h1>Events for Good</h1>
   <div class="events">
-    <!-- <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
-  <div class="events">
-    <!-- <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <div class="category">
     <Categories v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
@@ -16,58 +12,20 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
 import Categories from '@/components/Categories.vue'
-import axios from 'axios'
+import EventService from '@/services/EventService.js'
 export default {
   name: 'EventListView',
   components: {
-    //   HelloWorld
-    // }
-    EventCard, //register it as a child component
+    EventCard,
     Categories
   },
   data() {
     return {
-      // events: [
-      //   {
-      //     id: 5928101,
-      //     category: 'animal welfare',
-      //     title: 'Cat Adoption Day',
-      //     description: 'Find your new feline friend at this event.',
-      //     location: 'Meow Town',
-      //     date: 'January 28, 2022',
-      //     time: '12:00',
-      //     petsAllowed: true,
-      //     organizer: 'Kat Laydee'
-      //   },
-      //   {
-      //     id: 4582797,
-      //     category: 'food',
-      //     title: 'Community Gardening',
-      //     description: 'Join us as we tend to the community edible plants.',
-      //     location: 'Flora City',
-      //     date: 'March 14, 2022',
-      //     time: '10:00',
-      //     petsAllowed: true,
-      //     organizer: 'Fern Pollin'
-      //   },
-      //   {
-      //     id: 8419988,
-      //     category: 'sustainability',
-      //     title: 'Beach Cleanup',
-      //     description: 'Help pick up trash along the shore.',
-      //     location: 'Playa Del Carmen',
-      //     date: 'July 22, 2022',
-      //     time: '11:00',
-      //     petsAllowed: false,
-      //     organizer: 'Carey Wales'
-      //   }
-      // ]
       events: null
     }
   },
   created() {
-    axios
-      .get('http://localhost:3004/events')
+    EventService.getEvents()
       .then((response) => {
         this.events = response.data
       })
@@ -80,6 +38,15 @@ export default {
 <style scoped>
 .events {
   display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+h4 {
+  font-size: 20px;
+}
+.category {
+  display: flex;
+  text-align: right;
   flex-direction: column;
   align-items: center;
 }

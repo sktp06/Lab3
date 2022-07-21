8cdfa77a57bb06e9ff05a6bcd60be3fcd3190773
sktp06 <kantaporn_s@cmu.ac.th>
2022-07-21 22:40:57 +0700

    5.4

--- lab3/src/router/index.js
+++ lab3/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const routes = [
   {
@@ -13,8 +14,9 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
   }
 ]
 

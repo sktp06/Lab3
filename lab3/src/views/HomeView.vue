1d820db8f36602c61fd3ca49b579b370a07ac8e4
sktp06 <kantaporn_s@cmu.ac.th>
2022-07-21 21:46:40 +0700

    4.6

--- lab3/src/views/HomeView.vue
+++ lab3/src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <EventCard/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import EventCard from '@/components/EventCard.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+  //   HelloWorld
+  // }
+  EventCard, //register it as a child component
   }
 }
 </script>

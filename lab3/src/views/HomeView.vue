72d34651c8a4547b53b96ea5e868ccf20f775169
sktp06 <kantaporn_s@cmu.ac.th>
2022-07-21 21:56:39 +0700

    4.9

--- lab3/src/views/HomeView.vue
+++ lab3/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <EventCard/>
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
   </div>
 </template>
 
